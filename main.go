adbc6e9d5e123167120a8750eac1cfd4fba62c86
Philip Kazakov <fliptv97@gmail.com>
2025-01-25 14:29:38 +0200

    chapter 1

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	commands := getCommands()
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		parts := cleanInput(scanner.Text())
+
+		if len(parts) == 0 {
+			continue
+		}
+
+		commandName := parts[0]
+		command, ok := commands[commandName]
+
+		if !ok {
+			fmt.Println("Unknown command")
+
+			continue
+		}
+
+		command.callback()
+	}
+}
+
+func cleanInput(text string) []string {
+	return strings.Fields(strings.ToLower(text))
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
